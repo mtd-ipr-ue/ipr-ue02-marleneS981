b458663830e39d0bdc1b6f37c6862a461b19b3c8
marleneS981 <an.marlene.schumacher@gmail.com>
2024-11-03 11:30:14 +0100

    UE2_1_fertig

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,25 @@
 import libraries.Out;
+import libraries.In;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Out.print("Enter first number: ");
+        int firstNumber = In.readInt();
+
+        Out.print("Enter second number: ");
+        int secondNumber = In.readInt();
+
+        Out.print("Enter third number: ");
+        int thirdNumber = In.readInt();
+
+        int max = firstNumber;
+        if (secondNumber > max) {
+            max = secondNumber;
+        }
+        if (thirdNumber > max) {
+            max = thirdNumber;
+        }
+
+        Out.println("The maximum of the three numbers is: " + max);
     }
 }
